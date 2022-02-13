a691bb27ecc6318194345ca4dca419f3d7b38291
Salatio <99189453+Salatio@users.noreply.github.com>
2022-02-13 18:29:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,8 +8,7 @@ int gcd(int a, int b) {
     x = b;
     int max = 1;
     for (int i = 1; i <= x; ++i) {
-        if ((0 == a % i)&&(0 == b % i))
-	max = i;
+        if ((0 == a % i)&&(0 == b % i))	max = i;
     }
     return max;
 }
